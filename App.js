f620d7b8db97718d08290515e64a5b70cface5d6
Anthony Thompson <anthony.steven.thompson@hpe.com>
2018-04-19 16:35:05 -0700

    Working sample app that compiles to Native (ios/android) and Web.

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import Hello from "./components/Hello";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View id="sobeawrapper" style={styles.wrapper}>
+        <View id="superbro" style={styles.container}>
+          <Hello />
+          <Text>Duck Eggs for Everyone!</Text>
+          <Text>Meridian Web Components</Text>
+          <Text>Platform: {`${Platform.OS}, right?`}</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
 });
---
+++ components/Hello.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+class Hello extends Component {
+  render() {
+    return <Text>sup bro?</Text>;
+  }
+}
+
+export default Hello;

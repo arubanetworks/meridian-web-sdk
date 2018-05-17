33b98c7d0004c12a0ae1a80037af5811aef9a8a0
Anthony Thompson <anthony.steven.thompson@hpe.com>
2018-05-17 15:44:59 -0700

    POC events and lifecycle hooks

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -19,7 +19,7 @@ interface Tag {
 @Component({
   tag: "meridian-map",
   styleUrl: "map.css",
-  shadow: true
+  shadow: false
 })
 export class MeridianMap {
   @Element() el: HTMLElement;
@@ -27,16 +27,44 @@ export class MeridianMap {
   @Prop() locationId: string;
   @Prop() floorId: string;
 
+  @State() loaded: boolean;
   @State() currentText: string = "";
   @State() svgUrl: string = "";
   @State() tags: Record<string, Tag> = {};
   @State() connection: any;
+  @State() connectionStatus: string = "Not Connected";
+
+  closeConnection() {
+    console.info("calling close");
+    this.connection.close();
+    this.connectionStatus = "Closed";
+  }
+
+  initMapControls() {
+    console.info("init map controls");
+    console.info("this.el", this.el);
+    var element = this.el.querySelector(".svg_parent");
+    console.info("element", element);
+    // var foo = SVG.adopt(element);
+    // foo.panZoom({ zoomMin: 0.25, zoomMax: 20 });
+  }
+
+  componentDidUpdate() {
+    console.log("Component did update");
+  }
 
   async componentDidLoad() {
+    console.info("Component did load");
+
+    const { data } = await api.floor.get(this.locationId, this.floorId);
+    this.svgUrl = data.svg_url;
+    this.loaded = true;
+
     this.connection = api.floor.listen({
       locationId: this.locationId,
       id: this.floorId,
       onUpdate: data => {
+        this.connectionStatus = "Connected";
         const { mac } = data;
         const { x, y } = data.calculations.default.location;
         const tag = { mac, x, y };
@@ -44,19 +72,10 @@ export class MeridianMap {
       }
     });
     setTimeout(() => {
-      this.connection.close();
+      this.closeConnection();
     }, seconds(60));
 
-    const { data } = await api.floor.get(this.locationId, this.floorId);
-    this.svgUrl = data.svg_url;
-
-    setTimeout(() => {
-      console.info("component loaded");
-      var element = this.el.shadowRoot.querySelector(".map .svg_parent");
-      console.info("element", element);
-      var foo = SVG.adopt(element);
-      foo.panZoom({ zoomMin: 0.25, zoomMax: 20 });
-    }, seconds(1));
+    this.initMapControls();
   }
 
   renderTags() {
@@ -64,7 +83,6 @@ export class MeridianMap {
     return _.keys(tags).map(mac => {
       const t = tags[mac];
       const { x, y } = tags[mac];
-      // return <circle class="tag" cx={x} cy={y} r="10" />;
       return (
         <use
           fill="black"
@@ -79,24 +97,27 @@ export class MeridianMap {
   }
 
   render() {
-    if (this.svgUrl) {
-      return (
-        <svg class="map" viewBox="0 0 1700 2200">
-          <g class="svg_parent">
-            <image
-              id="svg_map_image"
-              width="1700"
-              height="2200"
-              xlinkHref={this.svgUrl}
-            />
-            {this.renderTags()}
-          </g>
-        </svg>
-      );
-    }
     return (
       <div>
-        <p>Loading...</p>
+        <p>
+          <button onClick={() => this.closeConnection()}>
+            Close Connection
+          </button>
+          <span> Status: {this.connectionStatus}</span>
+        </p>
+        <div>
+          <svg class="map" viewBox="0 0 1700 2200">
+            <g class="svg_parent">
+              <image
+                id="svg_map_image"
+                width="1700"
+                height="2200"
+                xlinkHref={this.svgUrl}
+              />
+              {this.renderTags()}
+            </g>
+          </svg>
+        </div>
       </div>
     );
   }

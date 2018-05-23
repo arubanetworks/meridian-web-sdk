5ac53cef87b2cb93853949ee7d8663f92403ee08
Anthony Thompson <anthony.steven.thompson@hpe.com>
2018-05-23 13:07:13 -0700

    more code

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,12 @@
 import { h, Component } from "preact";
+import _ from "lodash";
+
+const { MeridianWebApi } = global;
+const seconds = s => s * 1000;
 
 export default class Map extends Component {
   render() {
     const { name } = this.props;
-    return <div>I'm a map named {name}</div>;
+    return <div>I am a map named: {name}</div>;
   }
 }

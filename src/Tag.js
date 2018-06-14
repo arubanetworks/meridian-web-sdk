6af2ca15026ea2a79dd3ef9ed081cf5f38e381e4
Brian Mock <mock@hpe.com>
2018-06-14 10:24:54 -0700

    WIP

--- src/Tag.js
+++ src/Tag.js
@@ -1,80 +1,48 @@
 import { h } from "preact";
 import PropTypes from "prop-types";
 
-import { css, cx } from "./style";
+import { css, cx, mixins } from "./style";
 
-const cssCircleClip = css({
-  label: "meridian-svg-circle-clip",
-  clipPath: "circle()"
-});
-
-const cssTransitionXY = css({
-  label: "meridian-transition-xy",
+const cssTag = css({
+  label: "meridian-tag",
+  ...mixins.shadow,
+  borderRadius: "100%",
+  position: "absolute",
+  backgroundColor: "white",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  border: "2px solid white",
+  width: 24,
+  height: 24,
   transition: `
-    cx 500ms ease,
-    cy 500ms ease,
-    x 500ms ease,
-    y 500ms ease
-  `
+    top 500ms ease,
+    left 500ms ease
+  `,
+  "&:focus": {
+    zIndex: 1,
+    outline: "none",
+    boxShadow: "0 0 4px black"
+  }
 });
 
 const Tag = ({ x, y, data, onClick = () => {} }) => {
-  const imageUrl = data.image_url;
-  if (imageUrl) {
-    return (
-      <g cursor="pointer">
-        <circle
-          className={cssTransitionXY}
-          r="14"
-          cx={x + 14}
-          cy={y + 14}
-          fill="rgba(0, 0, 0, 0.2)"
-        />
-        <circle
-          className={cssTransitionXY}
-          r="13"
-          cx={x + 14}
-          cy={y + 14}
-          fill="white"
-        />
-        <image
-          className={cx(cssCircleClip, cssTransitionXY, "meridian-tag")}
-          width="24"
-          height="24"
-          x={x + 2}
-          y={y + 2}
-          preserveAspectRatio="xMidYMid slice"
-          pointerEvents="all"
-          xlinkHref={imageUrl}
-          onClick={onClick}
-        />
-      </g>
-    );
-  }
+  const imageURL = data.image_url;
+  const background = imageURL
+    ? { backgroundImage: `url('${imageURL}')` }
+    : { backgroundColor: "black" };
   return (
-    <g cursor="pointer">
-      <circle
-        className={cssTransitionXY}
-        r="14"
-        cx={x + 12}
-        cy={y + 12}
-        fill="rgba(0, 0, 0, 0.2)"
-      />
-      <circle
-        className={cssTransitionXY}
-        r="13"
-        cx={x + 12}
-        cy={y + 12}
-        fill="white"
-        onClick={onClick}
-      />
-      <use
-        className={cssTransitionXY}
-        x={x - 6}
-        y={y - 6}
-        href="#meridian-tag-default"
-      />
-    </g>
+    <div
+      aria-role="button"
+      tabIndex="0"
+      className={cx(cssTag, "meridian-tag")}
+      style={{
+        left: x,
+        top: y,
+        ...background
+      }}
+      onClick={onClick}
+    />
   );
 };
 

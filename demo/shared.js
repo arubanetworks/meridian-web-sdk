c8422857f2828c2b4217d29d0dd37b037faf7298
Brian Mock <mock@hpe.com>
2018-06-25 15:24:39 -0700

    cool

--- demo/shared.js
+++ demo/shared.js
@@ -1,33 +1,41 @@
-(function() {
+/* global $ */
+/* global hljs */
+
+$(function() {
   function showTheCode() {
-    var element = document.getElementById("the-code");
-    // TODO
+    function remove() {
+      dialog.remove();
+    }
+    var close1 = $("<button>")
+      .text("Close")
+      .addClass("close")
+      .on("click", remove);
+    var close2 = $("<button>")
+      .text("Close")
+      .addClass("close")
+      .on("click", remove);
+    var code = $("#the-code").text();
+    var html = hljs.highlight("javascript", code).value;
+    var dialog = $("<div>").addClass("dialog");
+    var pre = $("<pre>").html(html);
+    dialog.append(close1);
+    dialog.append(pre);
+    dialog.append(close2);
+    $("body").append(dialog);
   }
-
-  function main() {
-    // Container
-    var container = document.createElement("div");
-    container.className = "back";
-    // Back button
-    var back = document.createElement("a");
-    back.href = "../index.html";
-    back.innerHTML = "&larr; Back";
-    // Separator
-    var separator = document.createElement("span");
-    separator.className = "separator";
-    // Show the code button
-    var code = document.createElement("a");
-    code.innerHTML = "Show code";
-    code.href = "javascript:;";
-    code.onclick = function() {
-      showTheCode();
-    };
-    // Mount in the DOM
-    container.appendChild(back);
-    container.appendChild(separator);
-    container.appendChild(code);
-    document.body.appendChild(container);
-  }
-
-  document.addEventListener("DOMContentLoaded", main, false);
-})();
+  var container = $("<div>").addClass("back");
+  var back = $("<button>")
+    .text("Back")
+    .on("click", function() {
+      window.location = "../index.html";
+    });
+  var separator = $("<span>").addClass("separator");
+  var code = $("<button>")
+    .text("Code")
+    .on("click", showTheCode);
+  container
+    .append(back)
+    .append(separator)
+    .append(code);
+  $("body").append(container);
+});

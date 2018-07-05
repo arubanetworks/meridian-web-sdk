3a1c7b80d0b138a4e04ddd85ffced7a1003ec570
Brian Mock <mock@hpe.com>
2018-07-05 16:46:08 -0700

    for some reason it won't work if i try to use __SECRET__ in the URL
    i have to put it at the root of the domain for it to work? need to read more about GitHub pages

--- bin/deploy-demo.sh
+++ bin/deploy-demo.sh
@@ -8,13 +8,14 @@ clear=$'\e[0m'
 Heading() {
   echo "$clear"
   echo "$bold$cyan$@$clear"
+  echo "$clear"
 }
 
 # `npm run` provides this environment variable so we don't have to parse the
 # package.json just to get the version number
 version="${VERSION:-$npm_package_version}"
 name="${npm_package_name}"
-url="https://arubanetworks.github.io/meridian-web-sdk/__SECRET__"
+url="https://arubanetworks.github.io/meridian-web-sdk"
 
 Heading "--- Deploying $name demos v$version ---"
 
@@ -26,9 +27,9 @@ npm run -s build
 mv dist/meridian-sdk.js demo/
 
 Heading "* Copying build files..."
-npx gh-pages --dist demo --dest __SECRET__
+npx gh-pages --dist demo
 
 Heading "* Cleaning up build files..."
-rm -rf dist
+rm demo/meridian-sdk.js
 
 Heading "==> $url"

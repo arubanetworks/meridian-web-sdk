edb1dbeb942260dab8949c1310bd6e2713fe865f
Brian Mock <mock@hpe.com>
2018-07-09 10:53:38 -0700

    cleans up code dialog

--- demo/shared.js
+++ demo/shared.js
@@ -4,6 +4,22 @@
 /* global MeridianSDK */
 
 $(function() {
+  // Take code from a script tag and strip bad whitespace
+  function trimIndent(str) {
+    var match = str.match(/^[ \t]*(?=\S)/gm);
+    if (!match) {
+      return str;
+    }
+    var lengths = match.map(function(x) {
+      return x.length;
+    });
+    var indent = Math.min.apply(Math, lengths);
+    var re = new RegExp("^[ \\t]{" + indent + "}", "gm");
+    var s = indent > 0 ? str.replace(re, "") : str;
+    return s.trim();
+  }
+
+  // Show the performance monitor on screen
   function showPerf() {
     var script = document.createElement("script");
     script.onload = function() {
@@ -17,13 +33,12 @@ $(function() {
     script.src = "//rawgit.com/mrdoob/stats.js/master/build/stats.min.js";
     document.head.appendChild(script);
   }
+
+  // Show the code dialog
   function showTheCode() {
-    function remove() {
-      dialog.remove();
-    }
     var dialog = $("<div>").addClass("dialog flex flex-column");
     var heading = $("<div>")
-      .addClass("flex flex-center section")
+      .addClass("flex flex-center section top-bar")
       .appendTo(dialog);
     $("<div>")
       .text("Meridian SDK " + MeridianSDK.version)
@@ -32,9 +47,11 @@ $(function() {
     $("<button>")
       .text("Close")
       .addClass("close flex-0")
-      .on("click", remove)
+      .on("click", function() {
+        dialog.remove();
+      })
       .appendTo(heading);
-    var code = $("#the-code").text();
+    var code = trimIndent($("#the-code").text());
     var html = hljs.highlight("javascript", code).value;
     $("<pre>")
       .html(html)
@@ -42,23 +59,29 @@ $(function() {
       .appendTo(dialog);
     $("body").append(dialog);
   }
-  var container = $("<div>").addClass("back");
-  var back = $("<button>")
-    .text("Back")
-    .on("click", function() {
-      window.location = "../index.html";
-    });
-  var code = $("<button>")
-    .text("Code")
-    .on("click", showTheCode);
-  var perf = $("<button>")
-    .text("Perf")
-    .on("click", showPerf);
-  container
-    .append(back)
-    .append($("<span>").addClass("separator"))
-    .append(code)
-    .append($("<span>").addClass("separator"))
-    .append(perf);
-  $("body").append(container);
+
+  // Add the toolbar to each page
+  function addToolbar() {
+    var container = $("<div>").addClass("back");
+    var back = $("<button>")
+      .text("Back")
+      .on("click", function() {
+        window.location = "..";
+      });
+    var code = $("<button>")
+      .text("Code")
+      .on("click", showTheCode);
+    var perf = $("<button>")
+      .text("Perf")
+      .on("click", showPerf);
+    container
+      .append(back)
+      .append($("<span>").addClass("separator"))
+      .append(code)
+      .append($("<span>").addClass("separator"))
+      .append(perf);
+    $("body").append(container);
+  }
+
+  addToolbar();
 });

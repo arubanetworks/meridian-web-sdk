07d928d868f1862aeadb849df4236f44d331c649
Anthony Thompson <anthony.steven.thompson@hpe.com>
2018-08-02 11:45:56 -0700

    need to fetch ALL the floors

--- src/Map.js
+++ src/Map.js
@@ -127,7 +127,14 @@ export default class Map extends Component {
     const { locationID, api } = this.props;
     const mapURL = `locations/${locationID}/maps`;
     const { data } = await api.axios.get(mapURL);
-    return data.results;
+    const results = data.results;
+    let next = data.next;
+    while (next) {
+      const { data } = await api.axios.get(next);
+      results.push(...data.results);
+      next = data.next;
+    }
+    return results;
   }
 
   // TODO: We might want to memoize this based on floorID eventually

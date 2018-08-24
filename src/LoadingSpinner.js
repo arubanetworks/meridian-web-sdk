e7216c017873f5714153ec082fd83125fdeb42b4
Anthony Thompson <anthony.steven.thompson@hpe.com>
2018-08-24 16:08:47 -0700

    WS-2 Add css loading spinner (needs design review), enable emotion keyframes

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -1,6 +1,6 @@
 import { h } from "preact";
 
-import { css, theme, cx } from "./style";
+import { css, theme, cx, keyframes } from "./style";
 
 const cssLoadingSpinner = css({
   label: "spinner",
@@ -8,15 +8,30 @@ const cssLoadingSpinner = css({
   color: theme.textColor,
   position: "absolute",
   left: 0,
-  top: "48%",
+  top: "45%",
   right: 0,
   zIndex: 3,
   textAlign: "center"
 });
 
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(359deg); }
+`;
+
+const cssSpinner = css({
+  width: 28,
+  height: 28,
+  margin: "0 auto",
+  border: "4px rgba(0,0,0,0.25) solid",
+  borderTop: "4px rgba(0,0,0,1) solid",
+  borderRadius: "50%",
+  animation: `${spin} .6s infinite linear`
+});
+
 const LoadingOverlay = () => (
   <div className={cx(cssLoadingSpinner, "meridian-loading-spinner")}>
-    this is a spinner
+    <div className={cx(cssSpinner, "meridian-spinner")} />
   </div>
 );
 

950eb504d68b36f3459f55dbfa3c2cacd37ef4ea
Erik Harper <eriklharper@gmail.com>
2020-01-03 11:25:33 -0800

    Moving css function calls outside the component to improve performance

--- src/Placemark.js
+++ src/Placemark.js
@@ -5,6 +5,8 @@ import { getPlacemarkIconURL } from "./util";
 import { css, cx, mixins, theme } from "./style";
 import IconYouAreHere from "./IconYouAreHere";
 
+const SIZE = 24;
+
 const Placemark = ({
   isSelected,
   data,
@@ -13,63 +15,8 @@ const Placemark = ({
   disabled = false,
   youAreHerePlacemarkID
 }) => {
-  const SIZE = 24;
   const SHRINK_POINT = 0.2;
   const SHRINK_FACTOR = 1.4;
-  const cssLabel = css(mixins.textStrokeWhite, {
-    label: "meridian-label",
-    marginLeft: "50%",
-    position: "absolute",
-    minWidth: 55,
-    maxWidth: 120,
-    fontSize: 14,
-    textAlign: "center",
-    paddingTop: 4,
-    color: "black",
-    userSelect: "none",
-    transform: "translate(-50%, 0)",
-    fontWeight: "bold",
-    visibility: "visible",
-    "&[hidden]": {
-      visibility: "hidden"
-    }
-  });
-  const cssLabelOnly = css({
-    label: "meridian-label-only",
-    textTransform: "uppercase",
-    color: "#666",
-    fontSize: 16
-  });
-  const cssPlacemark = css({
-    label: "meridian-placemark",
-    position: "absolute"
-  });
-  const cssPlacemarkIcon = css(
-    mixins.buttonReset,
-    mixins.pointer,
-    mixins.focusNone,
-    {
-      label: "meridian-placemark-icon",
-      transition: "width 80ms ease, height 80ms ease",
-      display: "block",
-      width: SIZE,
-      height: SIZE,
-      borderRadius: "100%",
-      backgroundColor: theme.brandBlue,
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      border: "2px solid transparent",
-      overflow: "hidden",
-      zIndex: 1
-    }
-  );
-  const cssPlacemarkIconSelected = css(cssPlacemarkIcon, {
-    zIndex: 3,
-    width: SIZE * 1.25,
-    height: SIZE * 1.25,
-    boxShadow: "0 0 4px black"
-  });
   const cssTypeName = `meridian-placemark-type-${data.type}`;
   const labelOnly = !data.type || data.type.indexOf("label_") === 0;
   const shrinkFactor = mapZoomFactor < SHRINK_POINT ? SHRINK_FACTOR : 1;
@@ -168,5 +115,60 @@ Placemark.propTypes = {
   disabled: PropTypes.bool,
   youAreHerePlacemarkID: PropTypes.string
 };
+
+const cssLabel = css(mixins.textStrokeWhite, {
+  label: "meridian-label",
+  marginLeft: "50%",
+  position: "absolute",
+  minWidth: 55,
+  maxWidth: 120,
+  fontSize: 14,
+  textAlign: "center",
+  paddingTop: 4,
+  color: "black",
+  userSelect: "none",
+  transform: "translate(-50%, 0)",
+  fontWeight: "bold",
+  visibility: "visible",
+  "&[hidden]": {
+    visibility: "hidden"
+  }
+});
+const cssLabelOnly = css({
+  label: "meridian-label-only",
+  textTransform: "uppercase",
+  color: "#666",
+  fontSize: 16
+});
+const cssPlacemark = css({
+  label: "meridian-placemark",
+  position: "absolute"
+});
+const cssPlacemarkIcon = css(
+  mixins.buttonReset,
+  mixins.pointer,
+  mixins.focusNone,
+  {
+    label: "meridian-placemark-icon",
+    transition: "width 80ms ease, height 80ms ease",
+    display: "block",
+    width: SIZE,
+    height: SIZE,
+    borderRadius: "100%",
+    backgroundColor: theme.brandBlue,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    border: "2px solid transparent",
+    overflow: "hidden",
+    zIndex: 1
+  }
+);
+const cssPlacemarkIconSelected = css(cssPlacemarkIcon, {
+  zIndex: 3,
+  width: SIZE * 1.25,
+  height: SIZE * 1.25,
+  boxShadow: "0 0 4px black"
+});
 
 export default Placemark;

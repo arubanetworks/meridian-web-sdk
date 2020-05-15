ac2138597d4d7f8ba175fc55841fd88e7bd06bc4
Brian Mock <mock@hpe.com>
2020-05-15 13:26:01 -0700

    Deploy from CI (#169)

--- src/web-sdk.tsx
+++ src/web-sdk.tsx
@@ -2,7 +2,7 @@
 
 /*!
  * @license
- * Copyright 2018 Hewlett Packard Enterprise Development LP
+ * Copyright 2020 Hewlett Packard Enterprise Development LP
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

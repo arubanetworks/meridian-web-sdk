854d6810f0843d6141a2b2d799bb285851ab861e
Brian Mock <mock@hpe.com>
2020-08-31 17:19:40 -0700

    EXPERIMENTAL: Trying to make placemark culling work well

--- src/PlacemarkLayer.js
+++ src/PlacemarkLayer.js
@@ -7,6 +7,7 @@
 
 import { h, Component } from "preact";
 import PropTypes from "prop-types";
+import sortBy from "lodash.sortby";
 
 import MapMarker from "./MapMarker";
 
@@ -49,9 +50,46 @@ export default class PlacemarkLayer extends Component {
   }
 
   cullMarkers(markers) {
-    // TODO: Perform culling based on collision detection of labels and also for
-    // performance reasons
-    return markers;
+    const { mapZoomFactor } = this.props;
+    // Step the zoom up into little chunks so we don't calculate a fully new
+    // layout several times per second while zooming, which can cause UI flicker
+    const step = 4;
+    const approxZoom = Math.floor(mapZoomFactor * step) / step;
+    const result = [];
+
+    function isLabel(marker) {
+      return marker.type.startsWith("label_");
+    }
+
+    // Rough check to see if two markers are a certain number of _screen pixels_
+    // away from each other. Remember that the (x, y) values are in world
+    // coordinates, not screen coordinates, so we have to multiply by the zoom
+    // factor (approximate in this case).
+    function collides(marker1, marker2) {
+      const dx = marker1.x - marker2.x;
+      const dy = marker1.y - marker2.y;
+      const distance = Math.sqrt(dx ** 2 + dy ** 2);
+      return approxZoom * distance < 50;
+    }
+
+    // Prioritize label placemarks first, then just order things by their center
+    // point since nothing else really makes sense.
+    const sortedMarkers = sortBy(
+      markers,
+      m => (isLabel(m) ? -1 : 1),
+      m => m.x,
+      m => m.y
+    );
+    for (const m1 of sortedMarkers) {
+      // Labels always show even if they overlap; then we check to make sure the
+      // new placemark we're going to show collides with any other placemark
+      // we've already decided to show.
+      if (isLabel(m1) || !result.some(m2 => collides(m1, m2))) {
+        result.push(m1);
+      }
+    }
+
+    return result;
   }
 
   render() {

63755574b026d2f3b883b3bc1c4c558e050a3167
Brian Mock <mock@hpe.com>
2020-09-02 14:36:06 -0700

    `inline: no-fallback` is problematic

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-"use strict";
-
 const path = require("path");
-const webpack = require("webpack");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const nodeExternals = require("webpack-node-externals");
 
-const Package = require("./package.json");
-
-const definePlugin = new webpack.DefinePlugin({
-  GLOBAL_VERSION: JSON.stringify(Package.version),
-});
-
-const common = {
+module.exports = {
   module: {
     rules: [
       {
@@ -25,66 +13,8 @@ const common = {
             },
           },
         ],
-        exclude: /node_modules/,
       },
     ],
   },
-  plugins: [definePlugin],
-  node: {
-    __dirname: true,
-    fs: "empty",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
   entry: path.resolve(__dirname, "src/main.js"),
-  output: {
-    filename: "meridian-sdk.js",
-    library: "MeridianSDK",
-    libraryTarget: "var",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    filename: "meridian-sdk.js",
-    clientLogLevel: "error",
-    contentBase: path.resolve(__dirname, "demo"),
-    overlay: true,
-    port: 3011,
-    stats: "errors-only",
-  },
 };
-
-// This bundles our code into one file but avoids bundling everything in
-// node_modules, so that consumers can use us like a normal npm package
-const npmConfig = {
-  ...common,
-  output: {
-    filename: "main.js",
-    libraryTarget: "commonjs",
-    path: path.resolve(__dirname, "dist"),
-  },
-  target: "node",
-  externals: [nodeExternals()],
-};
-
-const development = {
-  ...common,
-  // devtool: false
-  // devtool: "source-map"
-};
-
-// Build the browser JS bundle as well as the npm bundle
-const production = [common, npmConfig];
-
-const analyze = {
-  ...common,
-  plugins: [definePlugin, new BundleAnalyzerPlugin()],
-};
-
-const configs = {
-  development,
-  production,
-  analyze,
-};
-
-module.exports = (env) => configs[env];

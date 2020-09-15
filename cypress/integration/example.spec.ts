81442e837c7eb9a0a0aa096354ec6a73d5b6dd8e
Brian Mock <mock@hpe.com>
2020-09-15 14:42:58 -0700

    counts

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -4,7 +4,8 @@ describe("Example", () => {
     cy.get(".meridian-map-container").should("exist");
     cy.get(".meridian-floor-label").should("contain", "Aruba HQ – Floor 01");
     cy.get('[data-meridian-tag-id="A0E6F83810BB"]').should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 4);
     cy.get('[data-meridian-placemark-id="5653164804014080"]').should("exist");
-    //  TODO: Check the count of tags and placemarks on screen
+    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
   });
 });

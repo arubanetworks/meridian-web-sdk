cb9c2d2636c0fd47fd755c1f2ce601aaaed3dc5b
Brian Mock <mock@hpe.com>
2020-09-15 15:16:15 -0700

    prettier mock svg

--- demo/cypress/example/mock-svg.js
+++ demo/cypress/example/mock-svg.js
@@ -1,5 +1,13 @@
 export default `
 <svg width="7213" height="4363" xmlns="http://www.w3.org/2000/svg">
-  <rect fill="#ccc" x="0" y="0" width="7213" height="4363" />
+  <rect
+    x="0"
+    y="0"
+    width="7213"
+    height="4363"
+    fill="#eee"
+    stroke="#aaa"
+    stroke-width="40"
+  />
 </svg>
 `;

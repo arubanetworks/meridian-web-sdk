a2c49af02ea7c350609ab2d8678201b03591d4a4
Brian Mock <brian@wavebeem.com>
2020-09-24 16:04:58 -0700

    MR-625 test disable click

---
+++ cypress/integration/disable-click.spec.ts
@@ -0,0 +1,8 @@
+describe("Disable click", () => {
+  it("not allow clicking placemarks or tags based on a boolean", () => {
+    cy.visit("/cypress/disable-click");
+    // Test all tags and placemarks
+    cy.get("[data-meridian-tag-id]").should("be.disabled");
+    cy.get("[data-meridian-placemark-id]").should("be.disabled");
+  });
+});

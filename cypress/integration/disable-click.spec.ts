14d2677799b99de14b69c3a315fb1c8115cf5955
Brian Mock <brian@wavebeem.com>
2020-09-24 17:37:27 -0700

    test fixer

--- cypress/integration/disable-click.spec.ts
+++ cypress/integration/disable-click.spec.ts
@@ -2,6 +2,8 @@ describe("Disable click", () => {
   it("not allow clicking placemarks or tags based on a boolean", () => {
     cy.visit("/cypress/disable-click");
     // Test all tags and placemarks
+    cy.get("[data-meridian-tag-id]").should("have.length", 3);
+    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
     cy.get("[data-meridian-tag-id]").should("be.disabled");
     cy.get("[data-meridian-placemark-id]").should("be.disabled");
   });

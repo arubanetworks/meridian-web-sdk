8498b47de0ea3194a5e680418cf7e71061687000
Brian Mock <brian@wavebeem.com>
2020-09-24 17:51:35 -0700

    do i need the container check

--- cypress/integration/disable-click.spec.ts
+++ cypress/integration/disable-click.spec.ts
@@ -1,6 +1,7 @@
 describe("Disable click", () => {
-  it("not allow clicking placemarks or tags based on a boolean", () => {
+  it("should not allow clicking placemarks or tags based on a boolean", () => {
     cy.visit("/cypress/disable-click");
+    cy.get(".meridian-map-container").should("exist");
     // Test all tags and placemarks
     cy.get("[data-meridian-tag-id]").should("have.length", 3);
     cy.get("[data-meridian-placemark-id]").should("have.length", 31);

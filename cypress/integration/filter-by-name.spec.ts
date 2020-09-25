3b334c1374eca3d79cb6385aec31126ebc0636e4
Brian Mock <mock@hpe.com>
2020-09-25 13:18:39 -0700

    MR-623 adds filter by name test (#190)

---
+++ cypress/integration/filter-by-name.spec.ts
@@ -0,0 +1,9 @@
+describe("Filter by name", () => {
+  it("should only show data specified by tags.filter and placemarks.filter", () => {
+    cy.visit("/cypress/filter-by-name");
+    cy.get('[data-meridian-tag-id="546C0E032A87"]').should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 1);
+    cy.get('[data-meridian-placemark-id="5717271485874176"]').should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 1);
+  });
+});

941cc320f20137db819e9e47d4bbee70ff8db58d
Brian Mock <mock@hpe.com>
2020-09-25 16:50:43 -0700

    Update filter-update.spec.ts

--- cypress/integration/filter-update.spec.ts
+++ cypress/integration/filter-update.spec.ts
@@ -13,7 +13,7 @@ describe("Filter update", () => {
     cy.get('[data-meridian-tag-id="546C0E032A87"]').should("exist");
     cy.get("[data-meridian-tag-id]").should("have.length", 3);
     cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
-    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
+    cy.get("[data-meridian-placemark-id]").should("have.length", 32);
 
     getMeridianMap().then(meridianMap => {
       meridianMap.update({

b3470dea64118dfe9bd17f0f49200f021e6b96a7
Brian Mock <brian@wavebeem.com>
2020-09-25 16:11:56 -0700

    better

--- cypress/integration/filter-update.spec.ts
+++ cypress/integration/filter-update.spec.ts
@@ -2,16 +2,13 @@ import { MeridianMap } from "../../src/web-sdk";
 
 describe("Filter update", () => {
   it("should only show data specified by tags.filter and placemarks.filter", () => {
-    // We don't have any way to know when the Web SDK is done updating, so we
-    // just need to sleep briefly. Otherwise we'll run both `meridianMap.update`
-    // calls before any of the Cypress assertions fire, causing most of them to
-    // fail.
-    const sleep = 100;
     cy.visit("/cypress/filter-update");
+    // -------------------------------------------------------------------------
     cy.get('[data-meridian-tag-id="546C0E032A87"]').should("exist");
     cy.get("[data-meridian-tag-id]").should("have.length", 3);
     cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
     cy.get("[data-meridian-placemark-id]").should("have.length", 32);
+    // -------------------------------------------------------------------------
     cy.window().then(contentWindow => {
       // Having a hard time figuring out how to add `meridianMap` as a property
       // of the window here, so just faking it with `as any` :shrug:
@@ -24,26 +21,24 @@ describe("Filter update", () => {
           filter: placemark => placemark.name === "Coffee Bar"
         }
       });
-      cy.wait(sleep).then(() => {
-        cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
-        cy.get("[data-meridian-tag-id]").should("have.length", 1);
-        cy.get("[data-meridian-placemark-id='5717271485874176']").should(
-          "exist"
-        );
-        cy.get("[data-meridian-placemark-id]").should("have.length", 1);
-        cy.wait(sleep).then(() => {
-          meridianMap.update({
-            tags: {},
-            placemarks: {}
-          });
-          cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
-          cy.get("[data-meridian-tag-id]").should("have.length", 3);
-          cy.get("[data-meridian-placemark-id='5717271485874176']").should(
-            "exist"
-          );
-          cy.get("[data-meridian-placemark-id]").should("have.length", 32);
-        });
+    });
+    cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 1);
+    cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 1);
+    // -------------------------------------------------------------------------
+    cy.window().then(contentWindow => {
+      // Having a hard time figuring out how to add `meridianMap` as a property
+      // of the window here, so just faking it with `as any` :shrug:
+      const meridianMap: MeridianMap = (contentWindow as any).meridianMap;
+      meridianMap.update({
+        tags: {},
+        placemarks: {}
       });
     });
+    cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 3);
+    cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 32);
   });
 });
---
+++ cypress/support/types.d.ts
@@ -0,0 +1,7 @@
+// import { MeridianMap } from "../../src/web-sdk";
+
+// declare namespace Cypress {
+//   interface ApplicationWindow {
+//     meridianMap: MeridianMap;
+//   }
+// }

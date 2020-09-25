0e05afcaa0c5a0d936a4ea1d1dfdd9600ade5099
Brian Mock <mock@hpe.com>
2020-09-25 16:56:45 -0700

    Cypress filter update test (#194)

---
+++ cypress/integration/filter-update.spec.ts
@@ -0,0 +1,46 @@
+import { MeridianMap } from "../../src/web-sdk";
+
+function getMeridianMap(): Cypress.Chainable<MeridianMap> {
+  return cy.window().then(contentWindow => {
+    return (contentWindow as any).meridianMap;
+  });
+}
+
+describe("Filter update", () => {
+  it("should only show data specified by tags.filter and placemarks.filter", () => {
+    cy.visit("/cypress/filter-update");
+
+    cy.get('[data-meridian-tag-id="546C0E032A87"]').should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 3);
+    cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
+
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        tags: {
+          filter: tag => tag.name === "Jamboard - Blue"
+        },
+        placemarks: {
+          filter: placemark => placemark.name === "Coffee Bar"
+        }
+      });
+    });
+
+    cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 1);
+    cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 1);
+
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        tags: {},
+        placemarks: {}
+      });
+    });
+
+    cy.get("[data-meridian-tag-id='546C0E032A87']").should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 3);
+    cy.get("[data-meridian-placemark-id='5717271485874176']").should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
+  });
+});

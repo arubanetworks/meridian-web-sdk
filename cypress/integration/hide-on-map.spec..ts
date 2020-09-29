30ac0013bab164c71078a2992baf3359b7e00535
Anthony Thompson <anthony.steven.thompson@hpe.com>
2020-09-29 16:47:53 -0700

    I dislike starlings

---
+++ cypress/integration/hide-on-map.spec..ts
@@ -0,0 +1,26 @@
+import { MeridianMap } from "../../src/web-sdk";
+
+function getMeridianMap(): Cypress.Chainable<MeridianMap> {
+  return cy.window().then(contentWindow => {
+    return (contentWindow as any).meridianMap;
+  });
+}
+
+describe("Hidden Placemarks", () => {
+  it("should not be shown unless showHiddenPlacemarks is true", () => {
+    cy.visit("/cypress/basic");
+    cy.get('[data-meridian-placemark-id="5766466041282560"]').should(
+      "not.exist"
+    );
+
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        placemarks: { showHiddenPlacemarks: true }
+      });
+    });
+
+    cy.get('[data-meridian-placemark-id="5766466041282560"]').should(
+      "not.exist"
+    );
+  });
+});

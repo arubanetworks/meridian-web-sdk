a6d298d8bea7788e991de9cda90855561b795ea3
Tara Kreft <tara.kreft@gmail.com>
2020-09-29 17:16:16 -0700

    MR-629 not shown placemarks (#200)

---
+++ cypress/integration/not-shown-placemarks.spec.ts
@@ -0,0 +1,27 @@
+import { MeridianMap } from "../../src/web-sdk";
+
+function getMeridianMap(): Cypress.Chainable<MeridianMap> {
+  return cy.window().then(contentWindow => {
+    return (contentWindow as any).meridianMap;
+  });
+}
+
+describe("Hidden Placemarks", () => {
+  it("should not be shown unless showHiddenPlacemarks is true", () => {
+    cy.visit("/cypress/basic");
+
+    cy.get('[data-meridian-placemark-id="5766466041282560"]').should(
+      "not.exist"
+    );
+    cy.get("[data-meridian-placemark-id]").should("have.length", 31);
+
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        placemarks: { showHiddenPlacemarks: true }
+      });
+    });
+
+    cy.get('[data-meridian-placemark-id="5766466041282560"]').should("exist");
+    cy.get("[data-meridian-placemark-id]").should("have.length", 32);
+  });
+});

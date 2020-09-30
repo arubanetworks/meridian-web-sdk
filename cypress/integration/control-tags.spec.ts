e9c54c2233516ef378d00de28034a570f9d735b4
Anthony Thompson <anthony.steven.thompson@hpe.com>
2020-09-30 12:13:05 -0700

    use new helper

--- cypress/integration/control-tags.spec.ts
+++ cypress/integration/control-tags.spec.ts
@@ -1,4 +1,4 @@
-import { MeridianMap } from "../../src/web-sdk";
+import { getMeridianMap } from "./util/getMeridianMap";
 
 describe("Control Tags", () => {
   it("control tags should be hidden by default", () => {
@@ -13,8 +13,7 @@ describe("Control Tags", () => {
     cy.get("[data-meridian-tag-id]").should("have.length", 3);
     cy.get('[data-meridian-tag-id="546C0E014517"]').should("not.exist");
 
-    cy.window().then((contentWindow: any) => {
-      const meridianMap: MeridianMap = contentWindow.meridianMap;
+    getMeridianMap().then(meridianMap => {
       meridianMap.update({
         tags: { showControlTags: true }
       });
@@ -36,8 +35,7 @@ describe("Control Tags", () => {
     cy.get('[data-meridian-tag-id="546C0E014517"]').should("exist");
     cy.get("[data-meridian-tag-id]").should("have.length", 4);
 
-    cy.window().then((contentWindow: any) => {
-      const meridianMap: MeridianMap = contentWindow.meridianMap;
+    getMeridianMap().then(meridianMap => {
       meridianMap.update({
         tags: { showControlTags: false }
       });

b936f785da9dd833484e75a029bc972e34329ad6
Anthony Thompson <vercasson@gmail.com>
2020-09-30 12:14:17 -0700

    CypressCI tests, control-tags test update (#205)

--- cypress/integration/control-tags.spec.ts
+++ cypress/integration/control-tags.spec.ts
@@ -1,7 +1,29 @@
-import { MeridianMap } from "../../src/web-sdk";
+import { getMeridianMap } from "./util/getMeridianMap";
 
 describe("Control Tags", () => {
   it("control tags should be hidden by default", () => {
+    cy.visit("/cypress/basic");
+
+    cy.get(".meridian-map-container").should("exist");
+    cy.get(".meridian-floor-label").should(
+      "contain",
+      "Main Building – Floor 01"
+    );
+
+    cy.get("[data-meridian-tag-id]").should("have.length", 3);
+    cy.get('[data-meridian-tag-id="546C0E014517"]').should("not.exist");
+
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        tags: { showControlTags: true }
+      });
+    });
+
+    cy.get('[data-meridian-tag-id="546C0E014517"]').should("exist");
+    cy.get("[data-meridian-tag-id]").should("have.length", 4);
+  });
+
+  it("control tags should be visible", () => {
     cy.visit("/cypress/control-tags");
 
     cy.get(".meridian-map-container").should("exist");
@@ -13,8 +35,7 @@ describe("Control Tags", () => {
     cy.get('[data-meridian-tag-id="546C0E014517"]').should("exist");
     cy.get("[data-meridian-tag-id]").should("have.length", 4);
 
-    cy.window().then((contentWindow: any) => {
-      const meridianMap: MeridianMap = contentWindow.meridianMap;
+    getMeridianMap().then(meridianMap => {
       meridianMap.update({
         tags: { showControlTags: false }
       });

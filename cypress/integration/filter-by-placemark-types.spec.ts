fb3b7bd316412f5ef649e91a0e48edb97cb2468e
Brian Mock <mock@hpe.com>
2020-10-13 16:03:28 -0700

    MR-668 Adds placemarks.filter Cypress test (#212)

---
+++ cypress/integration/filter-by-placemark-types.spec.ts
@@ -0,0 +1,31 @@
+import { getMeridianMap } from "./util/getMeridianMap";
+
+describe("Filter by placemark types", () => {
+  it("should filter placemarks based on type using placemarks.filter", () => {
+    cy.visit("/cypress/basic");
+    // All 31 placemarks exist
+    cy.get(".meridian-placemark-icon").should("have.length", 31);
+    // Only show "cafe" placemarks
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        placemarks: {
+          filter: p => p.type === "cafe"
+        }
+      });
+    });
+    // Only one placemark exists, and it's a "cafe"
+    cy.get(".meridian-placemark-icon")
+      .should("have.length", 1)
+      .should("have.class", "meridian-placemark-type-cafe");
+    // Show all placemarks again
+    getMeridianMap().then(meridianMap => {
+      meridianMap.update({
+        placemarks: {
+          filter: undefined
+        }
+      });
+    });
+    // All 31 placemarks exist
+    cy.get(".meridian-placemark-icon").should("have.length", 31);
+  });
+});

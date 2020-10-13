2b97f1e200e62a4dc834d0268b1998d2d244dab7
Brian Mock <brian@wavebeem.com>
2020-10-13 16:55:17 -0700

    more!

--- cypress/integration/tag-search.spec.ts
+++ cypress/integration/tag-search.spec.ts
@@ -16,10 +16,36 @@ describe("Tag Search", () => {
       "have.length",
       2
     );
+    // Clear the search
+    cy.get("[data-testid=meridian--private--overlay-search]").clear();
+    // Assert all 5 tags show up again
+    cy.get("[data-testid^=meridian--private--overlay-tag-]").should(
+      "have.length",
+      5
+    );
+    // Type something so we can assert the search filter is removed when we
+    // close the overlay
+    cy.get("[data-testid=meridian--private--overlay-search]").type(
+      "nothing will ever match this really long search filter"
+    );
+    // Make sure we have no results found after typing that bad search
+    cy.get("[data-testid=meridian--private--map-overlay]").contains(
+      "No results found."
+    );
     // Close overlay
     cy.get("[data-testid=meridian--private--close-overlay]").click();
     // Open tags list
     cy.get("[data-testid=meridian--private--tag-control]").click();
+    // Assert the search filter has been cleared
+    cy.get("[data-testid=meridian--private--overlay-search]").should(
+      "have.value",
+      ""
+    );
+    // Assert all 5 tags show up again
+    cy.get("[data-testid^=meridian--private--overlay-tag-]").should(
+      "have.length",
+      5
+    );
     // Search for "Chad"
     cy.get("[data-testid=meridian--private--overlay-search]").type("Chad");
     // Click "Chad"

c7f5a6cd17092ef688196a9d62847beab063ef36
Brian Mock <brian@wavebeem.com>
2020-11-25 14:23:20 -0800

    upgrade cypress usage

--- cypress/integration/util/getMeridianMap.ts
+++ cypress/integration/util/getMeridianMap.ts
@@ -10,6 +10,10 @@ export function getMeridianMap(): Cypress.Chainable<
   // `meridianMap` property, but no matter what I do it won't work. So here we
   // override the type to `any` so that we can access the global variable.
   return cy.window().then((contentWindow: any) => {
-    return contentWindow.document.querySelector("meridian-map");
+    const elem = contentWindow.document.querySelector("meridian-map");
+    if (!elem) {
+      throw new Error("couldn't find <meridian-map>");
+    }
+    return elem;
   });
 }

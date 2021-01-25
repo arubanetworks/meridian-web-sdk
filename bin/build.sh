6dd7b87a1721ab0e860a16e057d766810e868f38
Brian Mock <brian@wavebeem.com>
2021-01-25 15:07:26 -0800

    fix the dest dir

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,7 @@ rm -rf dist docs
 mkdir -p dist docs
 npx webpack -p --mode production --env production
 npm run docs
-mkdir "$npm_package_version"
-mv dist/* "$npm_package_version"
-mv "$npm_package_version" dist
+dir="meridian-web-sdk/$npm_package_version"
+mkdir -p "$dir"
+mv dist/* "$dir"
+mv "$dir" dist

48055966765fd5201b8c93b86628a52f3d944923
Brian Mock <brian@wavebeem.com>
2021-01-25 14:13:06 -0800

    MR-974 upload files to GCS

--- bin/build.sh
+++ bin/build.sh
@@ -5,3 +5,6 @@ rm -rf dist docs
 mkdir -p dist docs
 npx webpack -p --mode production --env production
 npm run docs
+mkdir "$npm_package_version"
+mv dist/* "$npm_package_version"
+mv "$npm_package_version" dist

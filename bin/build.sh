5821b55c2bda94414cdbe427f75af03b5cc23874
Brian Mock <brian@wavebeem.com>
2021-01-25 15:17:36 -0800

    dir

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,7 @@ rm -rf dist docs
 mkdir -p dist docs
 npx webpack -p --mode production --env production
 npm run docs
-dir="meridian-web-sdk/$npm_package_version"
+dir="$npm_package_version"
 mkdir -p "$dir"
 mv dist/* "$dir"
 mv "$dir" dist

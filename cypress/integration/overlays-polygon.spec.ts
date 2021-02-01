cf68f636105a5c42a5ae8a50e0ac40428ff39587
Brian Mock <brian@wavebeem.com>
2021-02-01 13:00:39 -0800

    more thorough testing

--- cypress/integration/overlays-polygon.spec.ts
+++ cypress/integration/overlays-polygon.spec.ts
@@ -9,14 +9,23 @@ describe("Overlays: Polygon", () => {
         overlays: [
           {
             type: "polygon",
-            points: [400, 100, 800, 600, 100, 800]
+            points: [400, 100, 800, 600, 100, 800],
+            fill: "green",
+            stroke: "red",
+            strokeLineJoin: "round",
+            strokeWidth: 4
           }
         ]
       });
     });
-    cy.get(
-      "[data-testid=meridian--private--overlay-layer] > path:only-child"
-    ).should("have.attr", "d", "M400,100 L800,600 L100,800 Z");
+    cy.get("[data-testid=meridian--private--overlay-layer] > path:only-child")
+      .should("have.attr", "d", "M400,100 L800,600 L100,800 Z")
+      .should("have.attr", "fill", "green")
+      .should("have.attr", "stroke", "red")
+      .should("have.attr", "stroke-linejoin", "round");
+    // We can't test strokeWidth because it's scaled by the zoom level, so the
+    // values are kinda finicky for testing (i.e. if the zoom level changed
+    // slightly, the stroke-width on the path would change.)
   });
 
   it("should render a polygon from createMap, then render area placemarks", () => {

584668c7afd7297db3e1f26a257b1d10fa115612
Brian Mock <brian@wavebeem.com>
2021-02-09 16:28:45 -0800

    Use snapshot data for demos

--- demo/shared.js
+++ demo/shared.js
@@ -1,103 +1,77 @@
 /* global $ */
 /* global hljs */
-/* global Stats */
 /* global MeridianSDK */
 
-$(function() {
-  // Take code from a script tag and strip bad whitespace
-  function trimIndent(str) {
-    var match = str.match(/^[ \t]*(?=\S)/gm);
-    if (!match) {
-      return str;
-    }
-    var lengths = match.map(function(x) {
-      return x.length;
+import { fakeAPI } from "./cypress/utils/fake-api.js";
+
+if (MeridianSDK) {
+  // This uses a regular function, not an arrow function, so that we can safely
+  // use `new` with it.
+  MeridianSDK.API = function() {
+    return fakeAPI;
+  };
+} else {
+  // eslint-disable-next-line no-console
+  console.error("Failed to monkey patch fakeAPI into MeridianSDK");
+}
+
+// Take code from a script tag and strip bad whitespace
+function trimIndent(str) {
+  const match = str.match(/^[ \t]*(?=\S)/gm);
+  if (!match) {
+    return str;
+  }
+  const lengths = match.map(x => x.length);
+  const indent = Math.min(...lengths);
+  const re = new RegExp(`^[ \\t]{${indent}}`, "gm");
+  const s = indent > 0 ? str.replace(re, "") : str;
+  return s.trim();
+}
+
+// Show the code dialog
+function showTheCode() {
+  $("#meridian-map").hide();
+  const dialog = $("<div>").addClass("dialog flex flex-column");
+  const heading = $("<div>")
+    .addClass("flex flex-center section top-bar")
+    .appendTo(dialog);
+  $("<div>")
+    .text(`Meridian SDK ${MeridianSDK.version}`)
+    .addClass("heading flex-auto")
+    .appendTo(heading);
+  $("<button>")
+    .text("Close")
+    .addClass("close flex-0")
+    .on("click", () => {
+      dialog.remove();
+      $("#meridian-map").show();
+    })
+    .appendTo(heading);
+  const code = trimIndent($("#the-code").text());
+  const html = hljs.highlight("javascript", code).value;
+  $("<pre>")
+    .html(html)
+    .addClass("section")
+    .appendTo(dialog);
+  $("body").append(dialog);
+}
+
+// Add the toolbar to each page
+function addToolbar() {
+  const container = $("<div>").addClass("back");
+  const back = $("<button>")
+    .text("Back")
+    .on("click", () => {
+      window.location = "..";
     });
-    var indent = Math.min.apply(Math, lengths);
-    var re = new RegExp("^[ \\t]{" + indent + "}", "gm");
-    var s = indent > 0 ? str.replace(re, "") : str;
-    return s.trim();
+  const code = $("<button>")
+    .text("Code")
+    .on("click", showTheCode);
+  container.append(back);
+  if (window.location.pathname.indexOf("/docs/") === -1) {
+    container.append($("<span>").addClass("separator")).append(code);
   }
+  $("body").append(container);
+}
 
-  // Show the performance monitor on screen
-  var stats;
-  function initPerf() {
-    var url = "https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js";
-    $.getScript(url, function() {
-      stats = new Stats();
-      $(document.body).append(stats.dom);
-      requestAnimationFrame(function perfLoop() {
-        stats.update();
-        requestAnimationFrame(perfLoop);
-      });
-    });
-  }
-
-  function togglePerf() {
-    if (stats) {
-      $(stats.dom).toggle();
-    } else {
-      initPerf();
-    }
-  }
-
-  function hidePerf() {
-    if (stats) {
-      $(stats.dom).hide();
-    }
-  }
-
-  // Show the code dialog
-  function showTheCode() {
-    hidePerf();
-    var dialog = $("<div>").addClass("dialog flex flex-column");
-    var heading = $("<div>")
-      .addClass("flex flex-center section top-bar")
-      .appendTo(dialog);
-    $("<div>")
-      .text("Meridian SDK " + MeridianSDK.version)
-      .addClass("heading flex-auto")
-      .appendTo(heading);
-    $("<button>")
-      .text("Close")
-      .addClass("close flex-0")
-      .on("click", function() {
-        dialog.remove();
-      })
-      .appendTo(heading);
-    var code = trimIndent($("#the-code").text());
-    var html = hljs.highlight("javascript", code).value;
-    $("<pre>")
-      .html(html)
-      .addClass("section")
-      .appendTo(dialog);
-    $("body").append(dialog);
-  }
-
-  // Add the toolbar to each page
-  function addToolbar() {
-    var container = $("<div>").addClass("back");
-    var back = $("<button>")
-      .text("Back")
-      .on("click", function() {
-        window.location = "..";
-      });
-    var code = $("<button>")
-      .text("Code")
-      .on("click", showTheCode);
-    var perf = $("<button>")
-      .text("Perf")
-      .on("click", togglePerf);
-    container.append(back);
-    if (window.location.pathname.indexOf("/docs/") === -1) {
-      container
-        .append($("<span>").addClass("separator"))
-        .append(code)
-        .append($("<span>").addClass("separator"))
-        .append(perf);
-    }
-    $("body").append(container);
-  }
-
-  addToolbar();
-});
+addToolbar();

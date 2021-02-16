9c44ce3015beb01d4ebdd2c44269be3e10747497
Brian Mock <brian@wavebeem.com>
2021-02-16 12:57:15 -0800

    cypress fix

--- cypress/integration/overlays-polygon.spec.ts
+++ cypress/integration/overlays-polygon.spec.ts
@@ -18,23 +18,25 @@ describe("Overlays: Polygon", () => {
         ]
       });
     });
-    cy.get("[data-testid=meridian--private--overlay-layer] > path:only-child")
+    cy.get(
+      "[data-testid=meridian--private--overlay-layer] > polygon:only-child"
+    )
       .should("have.attr", "d", "M400,100 L800,600 L100,800 Z")
       .should("have.attr", "fill", "green")
       .should("have.attr", "stroke", "red")
       .should("have.attr", "stroke-linejoin", "round");
     // We can't test strokeWidth because it's scaled by the zoom level, so the
     // values are kinda finicky for testing (i.e. if the zoom level changed
-    // slightly, the stroke-width on the path would change.)
+    // slightly, the stroke-width on the polygon would change.)
   });
 
   it("should render a polygon from createMap, then render area placemarks", () => {
     cy.visit("/cypress/polygon");
     cy.get("[data-testid='meridian--private--map-container']").should("exist");
     cy.get(
-      "[data-testid=meridian--private--overlay-layer] > path:only-child"
+      "[data-testid=meridian--private--overlay-layer] > polygon:only-child"
     ).should("have.attr", "d", "M400,100 L800,600 L100,800 Z");
-    cy.get("[data-testid=meridian--private--overlay-layer] > path").should(
+    cy.get("[data-testid=meridian--private--overlay-layer] > polygon").should(
       "have.length.above",
       1
     );

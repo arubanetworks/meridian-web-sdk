34a9d08f5aec22a8b098ea0f0a89bf30bccdf151
Anthony Thompson <vercasson@gmail.com>
2021-05-06 13:17:14 -0700

    MR-1185 chrome browser zoom annotation layer bug (#259)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Fixed
+
+- Chrome Zoom related issue that caused annotations to be misaligned
+  when the Zoom level was not 100%
+
 ## [0.19.0] - 2021-04-26
 
 ### Added
--- src/OverlayLayer.tsx
+++ src/OverlayLayer.tsx
@@ -40,17 +40,14 @@ const OverlayLayer: FunctionComponent<OverlayLayerProps> = ({
   );
 };
 
-// Chrome requires that SVGs be at least 1x1 in order for `overflow: visible` to
-// work, so we're also adding `point-events: none` so that you can't click
-// anything in this layer.
 const cssOverlay = css({
   label: "overlay-layer",
   position: "absolute",
   overflow: "visible",
   top: 0,
   left: 0,
-  width: 1,
-  height: 1,
+  right: 0,
+  bottom: 0,
   pointerEvents: "none"
 });
 

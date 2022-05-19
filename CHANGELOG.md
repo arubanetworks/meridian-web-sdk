f4f67fd02e6ff097d620e3c15e29e25c4b95fef1
Tara Kreft <tara.kreft@gmail.com>
2022-05-18 20:01:33 -0400

    Mrc 1660 cypress lat lng (#308)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+- Cypress test for Demo of lat/lng to Meridian Map points
+
 ## [1.4.0] - 2021-05-17
 
 ### Changed
---
+++ cypress/integration/annotation-overlay-latLng.spec.ts
@@ -0,0 +1,21 @@
+describe("Overlay: Point & Polygon by Lat/Lng", () => {
+  it("should render an annotation point named test by lat/lng", () => {
+    cy.visit("/cypress/annotation-point-lat-lng");
+
+    cy.get("[data-testid=meridian--private--annotation-layer]").should("exist");
+    cy.get("[data-testid=meridian--private--annotation-point]")
+      .contains("test")
+      .should("exist");
+  });
+  it("should render a polygon created by lat/lng points", () => {
+    cy.get(
+      "[data-testid=meridian--private--overlay-layer] > polygon:only-child"
+    )
+      .should(
+        "have.attr",
+        "points",
+        "2676.3496684757583,3981.5584417855234 863.8948290507998,3826.4073142284496 934.9714894979141,2927.8229486145065 2676.3496684757583,3735.902471996899"
+      )
+      .should("have.attr", "stroke", "hsl(207, 65%, 46%)");
+  });
+});

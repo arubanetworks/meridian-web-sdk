90e750df8c149db54055921996b641ca0b6dbb98
Anthony Thompson <anthony.steven.thompson@hpe.com>
2022-06-10 12:56:48 -0700

    maybe

--- bin/travis-cypress.sh
+++ bin/travis-cypress.sh
@@ -8,10 +8,11 @@ npm start >/dev/null 2>&1 &
 pid="$!"
 
 # Wait for it to start
-wait-on "http://localhost:3011"
+# wait-on "http://localhost:3011"
 
 # Run Cypress
-npx cypress run --quiet --headless --browser chrome
+# npx cypress run --quiet --headless --browser chrome
+npm start-server-and-test start http://localhost:3030 cy:run
 
 # Kill webpack-dev-server
 kill "$pid"

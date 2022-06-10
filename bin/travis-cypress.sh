5d1cb3f7d5ce2fa18c7310709d6f082fcac249f1
Anthony Thompson <anthony.steven.thompson@hpe.com>
2022-06-10 13:25:29 -0700

    fsdf

--- bin/travis-cypress.sh
+++ bin/travis-cypress.sh
@@ -2,16 +2,17 @@
 set -eu
 
 # Start webpack-dev-server in the background
-npm start >/dev/null 2>&1 &
+# npm start >/dev/null 2>&1 &
 
 # Store its PID
-pid="$!"
+# pid="$!"
 
 # Wait for it to start
-wait-on "http://localhost"
+# wait-on "http://localhost"
 
 # Run Cypress
-npx cypress run --quiet --headless --browser chrome
+# npx cypress run --quiet --headless --browser chrome
+npx start-server-and-test start http://localhost:3030 cy:run
 
 # Kill webpack-dev-server
-kill "$pid"
+# kill "$pid"

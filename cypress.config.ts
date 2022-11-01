8a9f102b94d1b9ea6c8506273b291b592bfbcb54
Anthony Thompson <anthony.steven.thompson@hpe.com>
2022-11-01 14:10:49 -0700

    update cy

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  e2e: {
+    baseUrl: "http://localhost:3011",
+    setupNodeEvents(_on, _config) {
+      // bind to the event we care about
+      // on("<event>", (arg1, arg2) => {
+      // plugin stuff here
+      // });
+    },
+  },
+});
